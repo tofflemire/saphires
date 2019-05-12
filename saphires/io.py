0c336246a0df39585d06104c2b70ff24de1c7d33
tofflemire <tofflemire@utexas.edu>
2019-05-12 17:18:32 -0500

    Allowing for a more general input

--- saphires/io.py
+++ saphires/io.py
@@ -1,5 +1,5 @@
 '''
-############################ SAPHIRES xc ##############################
+############################ SAPHIRES io ##############################
                      Written by Ben Tofflemire, 2019
 #######################################################################
 This file is part of the SAPHIRES python package.
@@ -14,7 +14,7 @@ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
 You should have received a copy of the MIT license with SAPHIRES.  
 If not, see <http://opensource.org/licenses/MIT>.
 
-Description:
+Module Description:
 A collection of functions that read in and out put spectra in the 
 SAPHIRES data format.
 '''
@@ -35,9 +35,9 @@ from saphires.extras import bspline_acr as bspl
 
 py_version = sys.version_info.major
 if py_version == 3:
-	nplts = 'U'	
+	nplts = 'U'	#the numph letter for a string
 if py_version == 2:
-	nplts = 'S'
+	nplts = 'S' #the numph letter for a string
 
 #For python 3
 #spec = pkl.load(open('lte05900-4.00-0.0.PHOENIX-ACES-AGSS-COND-2011-HiRes_2800-11000.p','rb'),encoding='bytes')
@@ -45,72 +45,155 @@ if py_version == 2:
 #For python 2
 #spec = pkl.load(open('lte05900-4.00-0.0.PHOENIX-ACES-AGSS-COND-2011-HiRes_2800-11000.p','rb'))
 
+#0  - ['nflux'] 		- native flux array (inverted)						
+#1  - ['nwave'] 		- native wavelength array							
+#2  - ['ndw'] 			- native wavelength spacing value					
+#3  - ['wav_cent'] 		- Order Central Wavelength							
+#4  - ['vflux'] 		- resampled flux array (inverted)					
+#5  - ['vwave'] 		- resampled wavelength array							
+#6  - ['vflux_temp'] 	- resampled template flux array (inverted)			
+#7  - ['vel'] 			- velocity array to be used with the BF				
+#8  - ['w_region'] 		- wavelength region									
+#9  - ['temp_name'] 	- template name										
+#10 - ['w_region_temp'] - template wavelength region							
+#11 - ['bf'] 			- broadening funciton								
+#12 - ['sbf'] 			- smoothed broadening function 						
+#13 - ['bf_fit_params'] - gaussian fit parameters							
+#14 - ['rv_shift'] 		- initial rv shift (used if you bf_compute_iter)		
+#15 - ['order_flag'] 	- order flag											
+#16 - ['w_region_iter'] - iter wavelength region								
+#17 - ['ccf_fit_params']- fit paramters of CCF								
 
-def target_pkl(spectra_list,w_file=None,combine_all=True,norm=True,w_mult=1.0,trim_style='clip',norm_w_width=200.0):
+
+def target_pkl(spectra_list,w_file=None,combine_all=True,norm=True,w_mult=1.0,
+               trim_style='clip',norm_w_width=200.0):
 	'''
-	A function to read in a target spectrum, or list of target spectra
-	with which to compute a broadening function. The template should be
-	continuum nornalized.
+	A function to read in a target spectrum, or list of target spectra, from 
+	a pickle dictionary with specific keywords, and put them into the 
+	SAPHIRES data structure.
+	
+	Required Keywords:
+	'wav'  - Contains a single or multidimensional (i.e. multiple orders) 
+			 wavelenth array. The wavelength array is assumed to be in 
+			 angstroms.
+	'flux' - Contains flux values corresponding to the wavelength array(s).
+	Both must have the same dimenisionality and length
+	
+	The "data structure" is nothing special, just a set of nested dictionaries,
+	but it is a simple way to keep all of the relevant information in one place
+	that is, hopefully, straight forward. 
 
-	This function is intended for use with pickled files that are a single
-	order. The opened pickle should be a dictionary for a single spectrum
-	with the a wavelength array in angstroms under the keyword "wav" and a 
-	flux array under the keyword "flux".
+	The returned data structure has two parts:
+	1) A list of dictionary keywords. They have the following form:
+	   "file_name[order][wavelength range]"
+	2) A dictionary. For each of the keywords above there is a nested
+	   dictionary with the following form:
+	   ['nflux'] 		- native flux array (inverted)
+	   ['nwave'] 		- native wavelength array
+	   ['ndw'] 			- native wavelength spacing va
+	   ['wav_cent'] 	- order central wavelength
+	   ['w_region'] 	- wavelength region
+	   ['rv_shift'] 	- initial rv shift (used if yo
+	   ['order_flag'] 	- order flag					
+	
+	Additional keywords and data are added to the nested dictionaries by 
+	other SAPHIRES functions, e.g.:
+	['vflux'] 			- resampled flux array (inverted)					
+	['vwave'] 			- resampled wavelength array							
+	['vflux_temp'] 		- resampled template flux array (inverted)			
+	['vel'] 			- velocity array to be used with the BF or CCF		
+	['temp_name'] 		- template name										
+	['bf'] 				- broadening funciton								
+	['sbf'] 			- smoothed broadening function 						
+	['bf_fit_params'] 	- gaussian fit parameters							
+	['rv_shift'] 		- initial rv shift (used if you bf_compute_iter)					
+	More details on this is various functions. 
 
-	"spectra_list" is a test file with one name of a pickle file per line
-	in the first column. In the second column you can define a range over
-	which to compute the broadening function. The convention is, 
-	"w1-w2,w3-w4", and you can have as many as you like.
-	If you want to use the entire wavelenth range, put a "*" in the second 
-	column.
+	Many of the spectral analysis techniques (cross correlation, broadening 
+	function, etc) work with a inverted spectrum, i.e. the continuum is at
+	zero and absorption lines extend upward - so all of the flux arrays are 
+	inverted in this way.
 
-	Set "tar_wnm" if the fits spacing are in nanometers instead of angstroms.
-	The program assume the native wavelength spacing is in angstroms. 
+	Parameters
+    ----------
+	spectra_list : 
 
-	This function fill return a list of the input file names as an array and
-	a dictionary where the name of each of the input files is a dictionary 
-	keyword. Each keyword will have following arrays assocated with it:
+		"spectra_list" is a test file with one name of a pickle file per line
+		in the first column. In the second column you can define a range over
+		which to compute the broadening function. The convention is, 
+		"w1-w2,w3-w4", and you can have as many as you like.
+		If you want to use the entire wavelenth range, put a "*" in the second 
+		column.
 
-	[0] - native flux array (inverted)						'nflux': t_flux,
-	[1] - native wavelength array							'nwave': t_w,
-	[2] - native wavelength spacing value					'ndw': t_dw,
-	[3] - Order Central Wavelength							'wav_cent': np.mean(t_w),
-	[4] - resampled flux array (inverted)					'vflux': 0,
-	[5] - resampled wavelength array						'vwave': 0,
-	[6] - resampled template flux array (inverted)			'vflux_temp': 0,
-	[7] - velocity array to be used with the BF				'vel': 0,
-	[8] - wavelength region									'w_region': w_range_out,
-	[9] - template name										'temp_name': 0,
-	[10]- template wavelength region						'w_region_temp': 0,
-	[11]- broadening funciton								'bf': 0,
-    [12]- smoothed broadening function 						'sbf': 0,
-    [13]- gaussian fit parameters							'bf_fit_params':0,
-	[14]- initial rv shift (used if you bf_compute_iter)	'rv_shift': 0,
-	[15]- order flag										'order_flag': 0,
-	[16]- iter wavelength region							'w_region_iter': '*',
-	[17]- fit paramters of CCF								'ccf_fit_params': 0}
+	w_file : bool
+		None
 
-	Most of these will be empty at the return of the this function but will 
-	be filled by the bf_compute and bf_analysis functions.
-	Idecies 4, 5, 6, 7, 9, 10 and 11 will be zeros.
+	combine_all : bool
+		Option to stitch together all spectral orders. This is useful generally,
+		but especially for low-S/N spectra where any given order could give you 
+		BF or CCF results that are trash. The default value is 'True'.
+
+	norm : bool
+		Option to continuum normalize the input spectrum. Default is True.
+
+	norm_w_width : float
+		If the 'norm' paramter is 'True', the parameter set the width of the
+		normalization window. The default is 200 Angstroms, which does a 
+		decent job in a variety of situtaions. 
+
+	w_mult : float
+		Value to multiply the wavelength array. This is used to convert the 
+		input wavelength array to Angstroms if it is not already. The default 
+		is 1, assuming the wavelength array is alreay in Angstroms. 
+
+	trim_style : str, options: 'clip', 'lin', 'spl'
+		If a wavelength region file is input in the 'spectra_list' parameter, 
+		this parameter describes how gaps are dealt with. 
+		- If 'clip', unused regions will be left as gaps.
+		- If 'lin', unused regions will be linearly interpolated over.
+		- If 'spl', unused regions will be interpolated over with a cubic 
+		  spline. You probably don't want to use this one.
+
+    Returns
+    -------
+	tar : array-like
+		List of dictionary keywords, described above.
+
+	tar_spec : dictionary
+		SAPHIRES dictionary, described above. 
 	'''
 
-	if w_file == None:
-		if (spectra_list[-2:] == 'ls') | (spectra_list[-2:] == 'xt'):
-			t_f_names=np.loadtxt(spectra_list,unpack=True,dtype='U10000')
-		if (spectra_list[-2:] == '.p'):
-			t_f_names = np.array(spectra_list,dtype=str)
+	in_type = spectra_list.split('.')[-1]
 
-		if (t_f_names.size == 1): 
-			t_f_names=np.array([t_f_names])
-
-	if w_file != None:
-		t_f_names,order,w_range = np.loadtxt(w_file,unpack=True,dtype=nplts+'10000,i,'+nplts+'10000')
+	if in_type in ['p','pkl']:
+		t_f_names = np.array([spectra_list])
+		order = np.array([0])
+		w_range = np.array(['*'])
+	else:
+		t_f_names,order,w_range = np.loadtxt(spectra_list,unpack=True,
+		                                     dtype=nplts+'10000,i,'+nplts+'10000')
 
 		if (t_f_names.size == 1): 
 			t_f_names=np.array([t_f_names])
 			order=np.array([order])
 			w_range=np.array([w_range])
+
+	#if w_file == None:
+	#	if (spectra_list[-2:] == 'ls') | (spectra_list[-2:] == 'xt'):
+	#		t_f_names=np.loadtxt(spectra_list,unpack=True,dtype=nplts+'10000')
+	#	if (spectra_list[-2:] == '.p'):
+	#		t_f_names = np.array(spectra_list,dtype=str)
+	#
+	#	if (t_f_names.size == 1): 
+	#		t_f_names=np.array([t_f_names])
+	#
+	#if w_file != None:
+	#	t_f_names,order,w_range = np.loadtxt(w_file,unpack=True,dtype=nplts+'10000,i,'+nplts+'10000')
+	#
+	#	if (t_f_names.size == 1): 
+	#		t_f_names=np.array([t_f_names])
+	#		order=np.array([order])
+	#		w_range=np.array([w_range])
 
 	#Dictionary for output spectra
 	t_spectra={}
@@ -188,7 +271,7 @@ def target_pkl(spectra_list,w_file=None,combine_all=True,norm=True,w_mult=1.0,tr
 										  'sbf': 0,
 										  'bf_fit_params':0,
 										  'rv_shift': 0,
-										  'order_flag': 0,
+										  'order_flag': 1,
 										  'w_region_iter': '*',
 										  'ccf_fit_params': 0}
 
